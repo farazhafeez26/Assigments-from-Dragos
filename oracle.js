0465217fda6a8a53e010f9618acc871c40de7ccc
farazhafeez <faraz.hafiz@gmail.com>
2020-08-30 19:24:57 +0200

    feedback implemented

--- oracle.js
+++ oracle.js
@@ -2,7 +2,7 @@ function myfunction1() {
     var x = confirm("Do you like sports?")
     y = confirm("Do you like pets?")
     z = confirm("Do you like pizza?")
-    if (x, y, z) {
+    if (x || y || z) {
         document.getElementById("demo").innerText = "You will have a nice life"
     }
 
