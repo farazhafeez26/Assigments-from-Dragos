0efb893bd1fa12983c2f3e51f6a7d8eb3eb57246
farazhafeez <faraz.hafiz@gmail.com>
2020-08-30 19:17:45 +0200

    homework oracle completed

---
+++ oracle.js
@@ -0,0 +1,12 @@
+function myfunction1() {
+    var x = confirm("Do you like sports?")
+    y = confirm("Do you like pets?")
+    z = confirm("Do you like pizza?")
+    if (x, y, z) {
+        document.getElementById("demo").innerText = "You will have a nice life"
+    }
+
+    else {
+        document.getElementById("demo").innerText = "Your life is doomed"
+    }
+}
